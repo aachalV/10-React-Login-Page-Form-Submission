42540ec93c403db38ca3c8c6cc11daaa89366be9
aachalv <aachal.vartak@raweng.com>
2021-01-07 20:01:22 +0530

    Frontend Validation Implemented

--- src/components/Forms/Signup.jsx
+++ src/components/Forms/Signup.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import Header from "../Header/Header";
 
 function Signup(props) {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setState((prevState) => ({ ...prevState, [id]: value }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (state.email.length) {
+      if (state.password === state.confirmPassword) {
+        //   sendDetailsToServer();
+        console.log("Password matched");
+      } else {
+        //   props.showError("Password does not match");
+        alert("Password does not match !!!");
+      }
+    } else {
+      alert("Enter valid Email");
+    }
+  };
   return (
     <>
       <Header heading={"Signup"} />
@@ -8,38 +32,48 @@ function Signup(props) {
         <div className="card col-12 col-lg-4 login-card my-5 ">
           <form className="my-3">
             <div className="form-group text-left">
-              <label htmlFor="exampleInputEmail1">Email address</label>
+              <label htmlFor="email">Email address</label>
               <input
                 type="email"
                 className="form-control"
                 id="email"
                 aria-describedby="emailHelp"
                 placeholder="Enter email"
+                value={state.email}
+                onChange={handleChange}
               />
               <small id="emailHelp" className="form-text text-muted">
                 We'll never share your email with anyone else.
               </small>
             </div>
             <div className="form-group text-left">
-              <label htmlFor="exampleInputPassword1">Password</label>
+              <label htmlFor="password">Password</label>
               <input
                 type="password"
                 className="form-control"
                 id="password"
                 placeholder="Password"
+                value={state.password}
+                onChange={handleChange}
               />
             </div>
             <div className="form-group text-left">
-              <label htmlFor="exampleInputPassword1">Confirm Password</label>
+              <label htmlFor="confirmPassword">Confirm Password</label>
               <input
                 type="password"
                 className="form-control"
                 id="confirmPassword"
                 placeholder="Confirm Password"
+                value={state.confirmPassword}
+                onChange={handleChange}
               />
             </div>
-            <button type="submit" className="btn btn-primary">
-              Register
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Submit
             </button>
           </form>
         </div>
