de6bb6f95941ae4acfdaa7b7dfe55fbf0a55371d
aachalv <aachal.vartak@raweng.com>
2021-01-07 15:19:47 +0530

    Header Implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Header from "../src/components/Header/Header";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <nav class="navbar navbar-dark bg-secondary">
+      <div className="row col-12 d-flex justify-content-center text-white">
+        <span className="h3">SignUp</span>
+      </div>
+    </nav>
+  );
+}
+export default Header;
